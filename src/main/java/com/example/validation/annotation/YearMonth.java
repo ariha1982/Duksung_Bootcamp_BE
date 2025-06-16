6afc12b2f8ebd3a93970337fd86d170635a0bfcb
Jayde1012 <ariha1982@naver.com>
2025-06-17 00:24:23 +0900

    Validation

---
+++ src/main/java/com/example/validation/annotation/YearMonth.java
@@ -0,0 +1,25 @@
+package com.example.validation.annotation;
+
+import com.example.validation.validator.YearMonthValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {YearMonthValidator.class})
+@NotBlank
+public @interface YearMonth {
+    String message() default "연월 양식에 맞지 않습니다 ex) 2025-06";
+    String pattern() default "yyyyMM";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
